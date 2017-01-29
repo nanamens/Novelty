a65aa1c1a6c87d2a5a6e87a939fd89617b43c89c
nanamens <nanaappiahmensah@yahoo.com>
2017-01-29 15:04:15 +0900

    Exit button

--- Novelty/Form1.vb
+++ Novelty/Form1.vb
@@ -1,5 +1,14 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.AcceptButton
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+
     End Sub
 End Class
